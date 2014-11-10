2510e726f507bcc24308fb1c570233a59f560685
javimuu <muuduy@gmail.com>
2014-11-10 22:26:30 +0700

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    @title = "Home"
+  end
+
+  def help
+    @title = "Help"
+  end
+
+  def about
+    @title = "About"
+  end
+
+  def contact
+    @title = "Contact"
+  end
+
+  def privecy
+    @title = "Privecy"
+  end
+end
